e854dd0472e2aca4cb0fd7fc8547cb4082e0b3cd
vkill <vkill.net@gmail.com>
2020-08-30 11:20:18 +0800

    i

--- google-androidpublisher-rest/src/v3/resources/purchases_products/product_purchase.rs
+++ google-androidpublisher-rest/src/v3/resources/purchases_products/product_purchase.rs
@@ -1,11 +1,9 @@
 use chrono::{DateTime, Utc};
-use serde::{Deserialize, Deserializer};
-use serde_aux::field_attributes::{
-    deserialize_datetime_utc_from_milliseconds, deserialize_number_from_string,
-};
+use serde::Deserialize;
+use serde_aux::field_attributes::deserialize_datetime_utc_from_milliseconds;
 use serde_repr::Deserialize_repr;
 
-use crate::v3::types::{price::Price, purchases_kind::PurchasesKind};
+use crate::v3::types::purchases_kind::PurchasesKind;
 
 #[derive(Deserialize, Debug)]
 pub struct ProductPurchase {
